e0625c05944f88668202b40ecc986da45f8bf95f
aadityajoshi151 <aadityajoshi151@gmail.com>
2020-11-20 11:52:10 +0530

    Update master.py

--- master.py
+++ master.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 import wget
 import time
+import os
+
 driver_path = "WebDriver/chromedriver.exe"
 driver = webdriver.Chrome(executable_path = driver_path)
-#todo Ask for download location in the starting.
+
+#todo Short desc and type to start
 #todo Remove unnecessary variables
+os.mkdir("Images")
 driver.get("http://www.facets.la/2013/1/")
 for i in range(1,11):
     daynum = driver.find_element_by_xpath('//*[@id="content"]/div[3]/p/span[1]/strong')
@@ -12,11 +16,11 @@ for i in range(1,11):
     try:
         downloadlink = driver.find_element_by_xpath('//*[@id="content"]/div[3]/div[9]/a')
         imageurl = downloadlink.get_attribute("href")
-        print(f"Day - {daynum.text}: {title.text} Downloading...\n")
+        print(f"\nDay - {daynum.text}: {title.text} Downloading...")
         filename = wget.download(imageurl, out = "Images/")
-        time.sleep(3)
+        time.sleep(2.5)
     except:  
-        print(f"Day - {daynum.text}: {title.text} Not Available For Download\n")
+        print(f"\nDay - {daynum.text}: {title.text} Not Available For Download")
     nextlink = driver.find_element_by_xpath('//*[@id="search-box-next"]/a')
     nextlink.click()
 driver.quit()
