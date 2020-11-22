8c7c4d7b8e9e5719aca3d06256643ee38a92d679
aadityajoshi151 <aadityajoshi151@gmail.com>
2020-11-22 11:54:43 +0530

    Update master.py

--- master.py
+++ master.py
@@ -6,7 +6,7 @@ import os
 
 
 print("""
-This bot is designed by Aaditya Joshi.
+This bot is designed by Aaditya Joshi (https://github.com/Aadityajoshi151).
 It is meant to automate the process of downloading the wallpapers made by the famous digital artist Justin Maller.
 It is only capable of downloading the wallpapers from http://www.facets.la/ that HAVE the download link on the website as some of the wallpapers do not.
 This bot is only used for automated downloading. Aaditya Joshi DOES NOT have the ownership of the website or the artwork present there.
@@ -23,7 +23,7 @@ driver = webdriver.Chrome(executable_path = driver_path)
 os.mkdir("Images")
 starttime = datetime.now()
 driver.get("http://www.facets.la/2013/1/")
-for i in range(1,366):
+for i in range(1,365):
     daynum = driver.find_element_by_xpath('//*[@id="content"]/div[3]/p/span[1]/strong').text
     title = driver.find_element_by_xpath('//*[@id="content"]/div[3]/h1').text
     try:
@@ -34,6 +34,8 @@ for i in range(1,366):
         time.sleep(1.75)
     except:  
         print(f'Day - {daynum}: "{title}" Not Available For Download\n')
+    if i+1 == 37:
+        driver.get("http://www.facets.la/2013/38/")
     if i < 365:
         nextlink = driver.find_element_by_xpath('//*[@id="search-box-next"]/a')
         nextlink.click()
